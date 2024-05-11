3455405ebc49fb7a59d6119b9b0a2242fa22bfd0
husaininazeer <husaini.nazeer@gmail.com>
2024-05-11 03:43:13 +0100

    Add About page from tsx

--- byquestionsui/src/main.tsx
+++ byquestionsui/src/main.tsx
@@ -5,12 +5,14 @@ import App from "./App.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
 import "./index.scss";
 import paths from "./paths.ts";
+import About from "./pages/About.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />} path={paths.basePages.home} />
+        <Route element={<About />} path={paths.basePages.about} />
         <Route element={<ErrorPage />} path="*" />
       </Routes>
     </BrowserRouter>
