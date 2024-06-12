062f99c2089de88f0c6175635838b8e4f954d046
husaininazeer <husaini.nazeer@gmail.com>
2024-06-12 15:37:30 +0100

    Add a projects page link to the NavBar

--- byquestionsui/src/main.tsx
+++ byquestionsui/src/main.tsx
@@ -6,12 +6,14 @@ import ErrorPage from "./pages/ErrorPage.tsx";
 import "./index.scss";
 import paths from "./paths.ts";
 import About from "./pages/About.tsx";
+import Projects from "./pages/Projects.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />} path={paths.basePages.home} />
+        <Route element={<Projects />} path={paths.basePages.projects} />
         <Route element={<About />} path={paths.basePages.about} />
         <Route element={<ErrorPage />} path="*" />
       </Routes>
